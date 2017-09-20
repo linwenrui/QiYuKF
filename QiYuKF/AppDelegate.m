3eefff27ef0e5a3500537522f5773e65e5a66a16
linwenrui <linwenrui@xiangha.com>
2017-09-20 16:52:22 +0800

    添加远程推送处理

--- QiYuKF/AppDelegate.m
+++ QiYuKF/AppDelegate.m
@@ -15,6 +15,7 @@
 
 @implementation AppDelegate
 
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
@@ -22,7 +23,34 @@
     [self customSessionUI];
     [[QYSDK sharedSDK] registerAppId:@"faf58ac0204f491676739f4de14eba97" appName:@"QiYuKF"];
     
+    // 推送消息相关处理
+    if ([application respondsToSelector:@selector(registerForRemoteNotifications)]) {
+        
+        UIUserNotificationType types = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert;
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [application registerUserNotificationSettings:settings];
+    } else {
+    
+        UIRemoteNotificationType types = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert;
+        [application registerForRemoteNotificationTypes:types];
+    }
+    
+    NSDictionary *remoteNotificationInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+    if (remoteNotificationInfo) {
+        
+        [self showChatViewController:remoteNotificationInfo];
+    }
+    
     return YES;
+}
+
+- (void)showChatViewController:(NSDictionary *)remoteNotificationInfo {
+
+    id object = [remoteNotificationInfo objectForKey:@"nim"]; // 含有"nim"字段,就标示是七鱼的消息
+    if (object) {
+        
+        NSLog(@"接收到七鱼的消息了");
+    }
 }
 
 #pragma mark - 自定义聊天组件
