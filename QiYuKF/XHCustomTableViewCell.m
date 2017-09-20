4175dd3c402e83d6758f5eba15e6913377a71604
linwenrui <linwenrui@xiangha.com>
2017-09-20 17:16:11 +0800

    对时间进行精确

--- QiYuKF/XHCustomTableViewCell.m
+++ QiYuKF/XHCustomTableViewCell.m
@@ -28,17 +28,47 @@
     self.headImg.image = [UIImage imageNamed:@"icon_customer_avatar"];
     self.titleLab.text = qySessionInfo.lastMessageText;
     self.unReadLab.text = [NSString stringWithFormat:@"%ld", qySessionInfo.unreadCount];
-    // 时间戳 -> 时间
-    // 格式化时间
-    NSDateFormatter *formatte = [[NSDateFormatter alloc] init];
-    formatte.timeZone = [NSTimeZone timeZoneWithName:@"beijing"];
-    [formatte setDateStyle:NSDateFormatterMediumStyle];
-    [formatte setTimeStyle:NSDateFormatterShortStyle];
-    [formatte setDateFormat:@"yyyy-MM-dd HH:mm"];
     
-    // 毫秒转为妙
-    NSDate *date = [NSDate dateWithTimeIntervalSince1970:qySessionInfo.lastMessageTimeStamp];
-    self.subLab.text = [formatte stringFromDate:date];
+    // 精确化时间
+    self.subLab.text = [self showTime:qySessionInfo.lastMessageTimeStamp];
+}
+
+- (NSString *)showTime:(NSTimeInterval)msglastTime {
+
+    // 今天的时间
+    NSDate *nowDate = [NSDate date];
+    NSDate *msgDate = [NSDate dateWithTimeIntervalSince1970:msglastTime];
+    NSString *result = nil;
+    NSCalendarUnit components = (NSCalendarUnit)(NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitWeekday | NSCalendarUnitHour | NSCalendarUnitMinute);
+    NSDateComponents *nowDateComponents = [[NSCalendar currentCalendar] components:components fromDate:nowDate];
+    NSDateComponents *msgDateComponents = [[NSCalendar currentCalendar] components:components fromDate:msgDate];
+    NSDate *today = [[NSDate alloc] init];
+    NSTimeInterval secondsDay = 24 * 60 * 60;
+    NSDate *yesterday = [today dateByAddingTimeInterval:-secondsDay];
+    NSDateComponents *yesterdayDateComponents = [[NSCalendar currentCalendar] components:components fromDate:yesterday];
+    NSInteger hour = msgDateComponents.hour;
+    result = @"";
+    
+    if (nowDateComponents.year == msgDateComponents.year
+        && nowDateComponents.month == msgDateComponents.month
+        && nowDateComponents.day == msgDateComponents.day) { // 今天, hh:mm
+    
+        result = [[NSString alloc] initWithFormat:@"%@ %zd:%02d", result, hour, (int)msgDateComponents.minute];
+    } else if (yesterdayDateComponents.year == msgDateComponents.year
+               && yesterdayDateComponents.month == msgDateComponents.month
+               && yesterdayDateComponents.day == msgDateComponents.day) { // 昨天, 昨天 hh:mm
+        
+        result = [[NSString alloc] initWithFormat:@"昨天%@ %zd:%02d", result, hour, (int)msgDateComponents.minute];
+    } else if (nowDateComponents.year == msgDateComponents.year) { // 今年, MM/dd hh:mm
+    
+        result = [NSString stringWithFormat:@"%02d/%02d %zd:%02d", (int)msgDateComponents.month, (int)msgDateComponents.day, msgDateComponents.hour, (int)msgDateComponents.minute];
+    } else if (nowDateComponents.year != msgDateComponents.year) { // 跨年, YY/MM/dd hh:mm
+    
+        NSString *day = [NSString stringWithFormat:@"%02d/%02d/%02d", (int)(msgDateComponents.year % 100), (int)msgDateComponents.month, (int)msgDateComponents.day];
+        result = [day stringByAppendingFormat:@" %@ %zd:%02d", result, hour, (int)msgDateComponents.minute];
+    }
+
+    return result;
 }
 
 + (NSString *)cellReuserIdentifier {
